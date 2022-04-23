70a4bed0ff63bafd8e00c6a584a9f2bec2b22fa1
Benjamin Nielsen <bluehippotech@gmail.com>
2022-04-23 23:52:43 +0200

    Add project files.

---
+++ PowerShellHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Management.Automation;
+using System.Text;
+
+namespace PowershellShowcase;
+
+public static class PowerShellHandler
+{
+    private static readonly PowerShell _ps = PowerShell.Create();
+
+    public static string Command(string script)
+    {
+        string errorMsg = string.Empty;
+
+        _ps.AddScript(script);
+
+        //Make sure return values are outputted to the stream captured by C#
+        _ps.AddCommand("Out-String");
+
+        PSDataCollection<PSObject> outputCollection = new();
+        _ps.Streams.Error.DataAdded += (object sender, DataAddedEventArgs e) =>
+        {
+            errorMsg = ((PSDataCollection<ErrorRecord>)sender)[e.Index].ToString();
+        };
+
+
+        IAsyncResult result = _ps.BeginInvoke<PSObject, PSObject>(null, outputCollection);
+
+        //Wait for powershell command/script to finish executing
+        _ps.EndInvoke(result);
+
+        StringBuilder sb = new();
+
+        foreach (var outputItem in outputCollection)
+        {
+            sb.AppendLine(outputItem.BaseObject.ToString());
+        }
+        
+        //Clears the commands we added to the powershell runspace so it's empty the next time we use it
+        _ps.Commands.Clear();
+
+        //If an error is encountered, return it
+        if (!string.IsNullOrEmpty(errorMsg))
+            return errorMsg;
+
+        return sb.ToString().Trim();
+    
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//https://www.youtube.com/channel/UC9qObYsgg5frlIwqdBjHlYw
+
+using PowershellShowcase;
+
+
+string systemInfo = PowerShellHandler.Command("Write-Output 'Blue-Hippo!'");
+
+Console.WriteLine(systemInfo);
+Console.ReadLine();
